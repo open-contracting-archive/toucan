8a495a3cc01e7e7d53523287543eedb872dcee14
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-09-25 17:33:54 -0400

    Use non-WEBDAV status code for request error

--- default/decorators.py
+++ default/decorators.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 def require_files(func):
     def wrap(request, *args, **kwargs):
         if 'files' not in request.session:
-            return JsonResponse({'error': 'No files uploaded'}, status=424, reason='No files available for operation')
+            return JsonResponse({'error': 'No files uploaded'}, status=400, reason='No files available for operation')
         return func(request, *args, **kwargs)
     wrap.__doc__ = func.__doc__
     wrap.__name__ = func.__name__
