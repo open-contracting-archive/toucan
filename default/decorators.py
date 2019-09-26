ae8d6cd758ff435858211c40e855b459e453eb18
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-09-26 16:56:16 -0400

    Use functools.wraps decorator

--- default/decorators.py
+++ default/decorators.py
@@ -1,11 +1,13 @@
+from functools import wraps
+
 from django.http import JsonResponse
 
 
-def require_files(func):
+def require_files(function):
+    @wraps(function)
     def wrap(request, *args, **kwargs):
         if 'files' not in request.session:
             return JsonResponse({'error': 'No files uploaded'}, status=400, reason='No files available for operation')
-        return func(request, *args, **kwargs)
-    wrap.__doc__ = func.__doc__
-    wrap.__name__ = func.__name__
+        return function(request, *args, **kwargs)
+
     return wrap
