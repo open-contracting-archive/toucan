7227e263ae816220ef83a148bed474c78dd0660f
Andres Aguilera <amaguilera96@gmail.com>
2019-11-05 16:57:11 -0300

    Test added

--- tests/test_package_releases.py
+++ tests/test_package_releases.py
@@ -7,6 +7,7 @@ class PackageReleasesTestCase(ViewTestCase, ViewTests):
         '1.1/releases/0001-tender.json',
         '1.1/releases/0001-award.json',
         '1.1/releases/0002-tender.json',
+        '1.1/releases/0003-array-tender.json',
     ]
 
     def test_go_with_files(self):
@@ -22,4 +23,5 @@ class PackageReleasesTestCase(ViewTestCase, ViewTests):
     def test_go_with_invalid_published_date(self):
         self.assertResults({'publishedDate': '2000-00-00T00:00:00Z'}, {
             'result.json': 'results/package-releases.json',
+
         })
