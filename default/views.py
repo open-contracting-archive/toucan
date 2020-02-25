9fb88c0aa10c8db4a16ae9e12764699461696a1a
Andres Aguilera <amaguilera96@gmail.com>
2020-02-25 07:55:44 -0300

    Delete about button

--- default/views.py
+++ default/views.py
@@ -47,10 +47,6 @@ def index(request):
     return render(request, 'default/index.html')
 
 
-def about(request):
-    return render(request, 'default/about.html')
-
-
 @clear_files
 def to_spreadsheet(request):
     return render(request, 'default/to-spreadsheet.html')
