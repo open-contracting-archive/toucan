720ac780eb7269800cae18170a5caaa9dd51cfdd
Andres Aguilera <amaguilera96@gmail.com>
2020-05-12 13:47:27 -0400

    Update tests

--- tests/test_upload_url.py
+++ tests/test_upload_url.py
@@ -4,17 +4,26 @@ from django.test import TestCase
 
 
 class UploadUrlTestCase(TestCase):
-    url = 'http://example.com'
+    urls = [
+        'http://example.com',
+        'http://example2.com',
+        'http://example3.com',
+        'badurl'
+    ]
 
-    @patch('default.views.os')
-    def test_upload_url(self, mock_os):
-        mock_os.stat = Mock(side_effect=FileNotFoundError())
-        response = self.client.post('/upload-url/', {'input_url': self.url})
+    def test_upload_url(self):
+        response = self.client.post('/upload-url/', {'input_url': self.urls[0]})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-Type'], 'application/json')
 
-    def test_bad_url(self):
-        bad_url = self.url.replace('.', "")
-        response = self.client.post('/upload-url/', {'input_url': bad_url})
+    def test_fail_upload_multiple_urls(self):
+        response = self.client.post('/upload-url/', {'input_url': self.urls})
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.content.decode('utf-8'), bad_url)
+        self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
+
+    @patch('default.views.os')
+    def test_folder_creation(self, mock_os):
+        mock_os.stat = Mock(side_effect=FileNotFoundError())
+        mock_os.mkdir = Mock(side_effect=FileNotFoundError())
+        with self.assertRaises(FileNotFoundError):
+            self.client.post('/upload-url/', {'input_url': self.urls[0]})
