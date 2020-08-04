80f95d27b4d10d532a6a5b9f2a8be2b7f972641a
Andres Aguilera <amaguilera96@gmail.com>
2020-08-04 14:48:57 -0400

    Update require_files decorator

--- default/decorators.py
+++ default/decorators.py
@@ -1,25 +1,53 @@
+import os
 from functools import wraps
+from zipfile import ZipFile
 
 from dateutil import parser
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.utils.translation import gettext as _
+
+from default.data_file import DataFile
+from default.util import invalid_request_file_message
 
 
 def clear_files(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
-        if 'clear' not in request.session or request.session['clear']:
-            request.session['files'] = []
+        request.session['files'] = []
         return function(request, *args, **kwargs)
 
     return wrap
 
 
-def require_files(function):
+def validate_files(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
         if 'files' not in request.session:
             return JsonResponse({'error': 'No files uploaded'}, status=400, reason='No files available for operation')
+
+        send_result = request.GET.get('sendResult')
+        if send_result:
+            # Set files session to the last generated results
+            for file in request.session['results']:
+                data_file = DataFile(**file)
+                # All json results are compressed in a zip file
+                if data_file.ext == '.zip':
+                    with ZipFile(data_file.path) as zipfile:
+                        for f in zipfile.infolist():
+                            prefix, ext = os.path.splitext(f.filename)
+                            new_file = DataFile(prefix, ext)
+                            path, f.filename = os.path.split(new_file.path)
+                            zipfile.extract(f, 'media/' + new_file.folder)
+                            # Open the file to check if it is the correct type
+                            with open(new_file.path, 'rb') as h:
+                                file_type = request.GET.get('type', None)
+                                message = invalid_request_file_message(h, file_type)
+                                if message:
+                                    return HttpResponse(message, status=401)  # error 401 for invalid type
+                                else:
+                                    request.session['files'].append(new_file.as_dict())
+            request.session.modified = True
+
         return function(request, *args, **kwargs)
 
     return wrap
---
+++ tests/test_send_results.py
@@ -0,0 +1,31 @@
+from tests import ViewTestCase, ViewTests
+
+
+class SendTestCase(ViewTestCase, ViewTests):
+    url = '/upgrade/'
+    send_to_url = '/to-spreadsheet/go/?sendResult=true&type=release-package'
+    files = [
+        '1.0/release-packages/0001-tender.json',
+    ]
+
+    def test_send_result(self):
+        self.url = '/upgrade/'
+        content = self.upload_and_go({'type': 'release-package'})
+
+        response = self.client.get(content['url'] + '?sendResult=true')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(self.send_to_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json')
+
+    def test_send_result_invalid_type(self):
+        self.url = '/compile/'
+        content = self.upload_and_go({'type': 'release-package'})
+
+        response = self.client.get(content['url'] + '?sendResult=true')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(self.send_to_url)
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.content, b'Not a release package')
